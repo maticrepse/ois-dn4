8f1222877fde991cee5c6d7b326a9c558880ed75
Matic Repše <matic.repse@gmail.com>
2014-12-21 22:06:59 +0000

    Konec naloge?

--- funkcionalnost.js
+++ funkcionalnost.js
@@ -35,6 +35,14 @@ setInterval(function enableButton(){
 	}else{
 		$("#vnesen").removeClass("disabled");
 	}
+
+	var inputMeritve = $("#selectMeritev").val();
+
+	if (!inputMeritve) {
+		$("#preberiMeritve").addClass("disabled");
+	}else{
+		$("#preberiMeritve").removeClass("disabled");
+	}
 }, 1);
 
 function vnesenEHRID() {
@@ -55,7 +63,27 @@ function vnesenEHRID() {
 				$("#profilID").html('<section class="container-fluid" id="section4"><div class="container"><div class="row"></div></div></section>');
 				console.log("Bolnik '" + party.firstNames + " " + party.lastNames + "', ki se je rodil '" + party.dateOfBirth + "'.");
 				$("#neviden").click();
-					
+				idProfila = ehrId;
+				imeProfila = party.firstNames;
+				priimekProfila = party.lastNames;
+				letoProfila = parseInt(party.dateOfBirth.substring(0,4));
+				mesecProfila = parseInt(party.dateOfBirth.substring(5,7));
+				danProfila = parseInt(party.dateOfBirth.substring(8,10));
+				today = new Date();
+				currentTime = today.getFullYear() + "-"+(today.getMonth()+1)+"-"+today.getDate()+"T"+today.getHours()+":"+today.getMinutes()+"Z";
+				spolProfila = party.gender;
+				if(spolProfila === "UNKNOWN"){
+					spolProfila= "Neznano";
+				}else if(spolProfila === "FEMALE"){
+					spolProfila= "Ženska";
+				}else if("OTHER" === spolProfila){
+					spolProfila="Drugo";
+				}else{
+					spolProfila="Moški";
+				}
+				
+				//$("#imeProfila").html('<h4 id="headerIme">&nbsp&nbsp&nbsp&nbsp' + imeProfila + '</h4>');
+				//vstaviPodatek("imeProfila", imeProfila, "headerIme");
 			},
 			error: function(err) {
 				$("#profilIDneuspesen").html('<div class="alert alert-dismissable alert-danger"><button type="button" class="close" data-dismiss="alert">×</button>Napaka "' + JSON.parse(err.responseText).userMessage + '!</div>');
@@ -65,6 +93,45 @@ function vnesenEHRID() {
 	}	
 }
 
+function vnesiPodatke() {
+    $("#imeProfila").html('<h4 id="headerIme">&nbsp&nbsp&nbsp&nbsp' + imeProfila + '</h4>');
+    $("#priimekProfila").html('<h4 id="headerIme">&nbsp&nbsp&nbsp&nbsp' + priimekProfila + '</h4>');
+    
+    $("#spolProfila").html('<h4 id="headerIme">&nbsp&nbsp&nbsp&nbsp' + spolProfila + '</h4>');
+    if(mesecProfila < (today.getMonth()+1)){
+    	letoProfila = today.getFullYear() - letoProfila;
+    }else if (danProfila <= today.getDate() ){
+    	letoProfila = today.getFullYear() - letoProfila;    	
+    }else{
+    	letoProfila = today.getFullYear() - letoProfila -1;    	
+    }
+    $("#starostProfila").html('<h4 id="headerIme">&nbsp&nbsp&nbsp&nbsp' + letoProfila + '</h4>');
+}
+
+function addChecked(cifra){
+	if(cifra === 1){
+		$("#optionsRadios1").addClass("checked");
+		$("#optionsRadios2").removeClass("checked");
+		$("#optionsRadios3").removeClass("checked");
+		$("#optionsRadios4").removeClass("checked");
+	}else if (cifra === 2){
+		$("#optionsRadios2").addClass("checked");
+		$("#optionsRadios1").removeClass("checked");
+		$("#optionsRadios3").removeClass("checked");
+		$("#optionsRadios4").removeClass("checked");
+	}else if (cifra === 3){
+		$("#optionsRadios3").addClass("checked");
+		$("#optionsRadios2").removeClass("checked");
+		$("#optionsRadios1").removeClass("checked");
+		$("#optionsRadios4").removeClass("checked");
+	}else{
+		$("#optionsRadios4").addClass("checked");
+		$("#optionsRadios2").removeClass("checked");
+		$("#optionsRadios3").removeClass("checked");
+		$("#optionsRadios1").removeClass("checked");
+	}
+}
+
 function ustvariEHR() {
 	
 	sessionId = getSessionId();
@@ -72,14 +139,18 @@ function ustvariEHR() {
 	var priimek = $("#inputLast").val();
 	var datumRojstva = $("#inputDatum").val();
 	var spol;
-	if($("#optionsRadios1").checked){
-		spol="MALE";
-	}else if($("#optionsRadios2").checked){
-		spol="FEMALE";
-	}else if($("#optionsRadios3").checked){
-		spol="OTHER";
-	}else{
+	if($("#optionsRadios4").hasClass("checked")){
 		spol="UNKNOWN";
+		console.log("unkw");
+	}else if($("#optionsRadios2").hasClass("checked")){
+		spol="FEMALE";
+		console.log("fml");
+	}else if($("#optionsRadios3").hasClass("checked")){
+		spol="OTHER";
+		console.log("othr");
+	}else{
+		spol="MALE";
+		console.log("ml");
 	}
 	if (!ime || !priimek || !datumRojstva || ime.trim().length == 0 || priimek.trim().length == 0 || datumRojstva.trim().length == 0) {
 		$("#vasID").html('<div class="alert alert-dismissable alert-danger"><button type="button" class="close" data-dismiss="alert">×</button>Prosimo vnesite podatke.</div>');
@@ -121,8 +192,182 @@ function ustvariEHR() {
 	}
 }
 
-function izpisiTezo(){
+function dodajMeritve() {
+	sessionId = getSessionId();
+
+
+	var datumInUra = currentTime;
+	var telesnaVisina = $("#inputVisina").val();
+	var telesnaTeza = $("#inputTeza").val();
+	var vMetrih = parseInt(telesnaVisina)/100;
+	var bodyMassIndex = parseInt(telesnaTeza)/(vMetrih*vMetrih);
 	
+	if(bodyMassIndex>25){
+		$("#prviVideo").empty();
+		$("#prviVideo").html('<div class="row"><div class="col-lg-10">&nbsp</div></div><div class="row"><div class="col-lg-4">&nbsp</div><div class="col-lg-4 alert alert-dismissable alert-danger"><button type="button" class="close" data-dismiss="alert">×</button><strong>Imate prekomerno telesno težo, predlagamo vam: </strong></div><div class="col-lg-4">&nbsp</div></div><div class="row"><div class="col-lg-4">&nbsp</div><div class="col-lg-4"><iframe width="420" height="315" src="https://www.youtube.com/v/qL210_mo0fI"></iframe></div></div>');
+		console.log("Video1");
+	}else if(bodyMassIndex<18.5){
+		$("#prviVideo").empty();
+		$("#prviVideo").html('<div class="row"><div class="col-lg-10">&nbsp</div></div><div class="row"><div class="col-lg-4">&nbsp</div><div class="col-lg-4 alert alert-dismissable alert-danger"><button type="button" class="close" data-dismiss="alert">×</button><strong>Imate premajhno telesno težo, predlagamo vam: </strong></div><div class="col-lg-4">&nbsp</div></div><div class="row"><div class="col-lg-4">&nbsp</div><div class="col-lg-4"><iframe width="420" height="315" src="https://www.youtube.com/v/DvFpT32VbFE"></iframe></div></div>');
+		console.log("Video2");
+	}else{
+		$("#prviVideo").empty();
+		$("#prviVideo").html('<div class="row"><div class="col-lg-10">&nbsp</div></div><div class="row"><div class="col-lg-4">&nbsp</div><div class="col-lg-4 alert alert-dismissable alert-success"><button type="button" class="close" data-dismiss="alert">×</button><strong>Imate idealen indeks telesne mase, svetujemo vam:</strong></div><div class="col-lg-4">&nbsp</div></div><div class="row"><div class="col-lg-4">&nbsp</div><div class="col-lg-4"><iframe width="420" height="315" src="https://www.youtube.com/v/K-Ch9kbtLYQ"></iframe></div></div>');
+		console.log("Video3");
+	}
+	$.ajaxSetup({
+		headers: {"Ehr-Session": sessionId}
+	});
+	var podatki = {
+		// Preview Structure: https://rest.ehrscape.com/rest/v1/template/Vital%20Signs/example
+		"ctx/language": "en",
+		"ctx/territory": "SI",
+		"ctx/time": datumInUra,
+		"vital_signs/height_length/any_event/body_height_length": telesnaVisina,
+		"vital_signs/body_weight/any_event/body_weight": telesnaTeza,
+		"vital_signs/body_mass_index/any_event/body_mass_index|magnitude": bodyMassIndex,
+		"vital_signs/body_mass_index/any_event/body_mass_index|unit":"kg/m2"
+		/*"vital_signs/body_temperature/any_event/temperature|magnitude": telesnaTemperatura,
+		"vital_signs/body_temperature/any_event/temperature|unit": "°C",
+		"vital_signs/blood_pressure/any_event/systolic": sistolicniKrvniTlak,
+		"vital_signs/blood_pressure/any_event/diastolic": diastolicniKrvniTlak,
+		"vital_signs/indirect_oximetry:0/spo2|numerator": nasicenostKrviSKisikom*/
+	};
+	var parametriZahteve = {
+		"ehrId": idProfila,
+		templateId: 'Vital Signs',
+		format: 'FLAT',
+		committer: imeProfila + " " + priimekProfila
+	};
+	$.ajax({
+		url: baseUrl + "/composition?" + $.param(parametriZahteve),
+		type: 'POST',
+		contentType: 'application/json',
+		data: JSON.stringify(podatki),
+		success: function (res) {
+			console.log(res.meta.href);
+			$("#dodajMeritveVitalnihZnakovSporocilo").html("<span class='obvestilo label label-success fade-in'>" + res.meta.href + ".</span>");
+		},
+		error: function(err) {
+			$("#dodajMeritveVitalnihZnakovSporocilo").html("<span class='obvestilo label label-danger fade-in'>Napaka '" + JSON.parse(err.responseText).userMessage + "'!");
+			console.log(JSON.parse(err.responseText).userMessage);
+		}
+	});
+
+}
+
+function izpisiTezo(){
+	sessionId = getSessionId();
+	var tip = $("#selectMeritev").val();
+	$.ajax({
+			url: baseUrl + "/demographics/ehr/" + idProfila + "/party",
+	    	type: 'GET',
+	    	headers: {"Ehr-Session": sessionId},
+	    	success: function (data) {
+				var party = data.party;
+				if(tip==="teza") {
+					$.ajax({
+						url: baseUrl + "/view/" + idProfila + "/" + "weight",
+						type: 'GET',
+						headers: {"Ehr-Session": sessionId},
+						success: function (res) {
+							if (res.length > 0) {
+								var results = "<table class='table table-striped table-hover'><tr><th>Datum in ura</th><th class='text-right'>Telesna teža</th></tr>";
+								for (var i in res) {
+									results += "<tr><td>" + res[i].time + "</td><td class='text-right'>" + res[i].weight + " " + res[i].unit + "</td>";
+								}
+								results += "</table>";
+								$("#preberiMeritveVitalnihZnakovSporocilo").empty();
+								$("#rezultatMeritveVitalnihZnakov").empty();
+								$("#rezultatMeritveVitalnihZnakov").append(results);
+							} else {
+								$("#rezultatMeritveVitalnihZnakov").empty();
+								$("#preberiMeritveVitalnihZnakovSporocilo").empty();
+								$("#preberiMeritveVitalnihZnakovSporocilo").html("<span class='obvestilo label label-warning fade-in'>Ni podatkov!</span>");
+							}
+						},
+						error: function (err) {
+							$("#rezultatMeritveVitalnihZnakov").empty();
+							$("#preberiMeritveVitalnihZnakovSporocilo").empty();
+							$("#preberiMeritveVitalnihZnakovSporocilo").html("<span class='obvestilo label label-danger fade-in'>Napaka '" + JSON.parse(err.responseText).userMessage + "'!");
+							console.log(JSON.parse(err.responseText).userMessage);
+						}
+					});
+				}else if(tip==="visina"){
+					$.ajax({
+						url: baseUrl + "/view/" + idProfila + "/" + "height",
+						type: 'GET',
+						headers: {"Ehr-Session": sessionId},
+						success: function (res) {
+							if (res.length > 0) {
+								var results = "<table class='table table-striped table-hover'><tr><th>Datum in ura</th><th class='text-right'>Telesna teža</th></tr>";
+								for (var i in res) {
+									results += "<tr><td>" + res[i].time + "</td><td class='text-right'>" + res[i].height + " " + res[i].unit + "</td>";
+								}
+								results += "</table>";
+								$("#preberiMeritveVitalnihZnakovSporocilo").empty();
+								$("#rezultatMeritveVitalnihZnakov").empty();
+								$("#rezultatMeritveVitalnihZnakov").append(results);
+							} else {
+								$("#rezultatMeritveVitalnihZnakov").empty();
+								$("#preberiMeritveVitalnihZnakovSporocilo").empty();
+								$("#preberiMeritveVitalnihZnakovSporocilo").html("<span class='obvestilo label label-warning fade-in'>Ni podatkov!</span>");
+							}
+						},
+						error: function (err) {
+							$("#rezultatMeritveVitalnihZnakov").empty();
+							$("#preberiMeritveVitalnihZnakovSporocilo").empty();
+							$("#preberiMeritveVitalnihZnakovSporocilo").html("<span class='obvestilo label label-danger fade-in'>Napaka '" + JSON.parse(err.responseText).userMessage + "'!");
+							console.log(JSON.parse(err.responseText).userMessage);
+						}
+					});
+				}else if(tip==="BMI"){
+					var AQL = 
+								"select "+
+								    "a_a/data[at0001]/events[at0002]/time/value as time, "+
+								    "a_a/data[at0001]/events[at0002]/data[at0003]/items[at0004]/value/magnitude as Body_Mass_Index_magnitude, "+
+								    "a_a/data[at0001]/events[at0002]/data[at0003]/items[at0004]/value/units as Body_Mass_Index_units "+
+								"from EHR e[e/ehr_id/value='" + idProfila + "'] " +
+								"contains COMPOSITION a "+
+								"contains OBSERVATION a_a[openEHR-EHR-OBSERVATION.body_mass_index.v1] "+
+								"where Body_Mass_Index_magnitude>25 or Body_Mass_Index_magnitude<18.5 "+
+								"order by time desc "+
+								"limit 10"
+					$.ajax({
+					    url: baseUrl + "/query?" + $.param({"aql": AQL}),
+					    type: 'GET',
+					    headers: {"Ehr-Session": sessionId},
+					    success: function (res) {
+					    	var results = "<table class='table table-striped table-hover'><tr><th>Datum in ura</th><th class='text-right'>Indeks telesne mase</th></tr>";
+					    	if (res) {
+					    		var rows = res.resultSet;
+						        for (var i in rows) {
+						        	if(rows[i].Body_Mass_Index_magnitude.toFixed(2)>25){
+						        		var stanje = "<span class='obvestilo label label-danger fade-in'>Prekomerna telesna teža!</span>"
+						        	}else{
+						        		var stanje = "<span class='obvestilo label label-danger fade-in'>Podhranjenost!</span>"
+						        	}
+						            results += "<tr><td>" + rows[i].time + "</td><td class='text-right'>"+stanje+"    " + rows[i].Body_Mass_Index_magnitude.toFixed(2) + " " 	+ rows[i].Body_Mass_Index_units + "</td>";
+						        }
+						        results += "</table>";
+						        $("#rezultatMeritveVitalnihZnakov").append(results);
+					    	} else {
+					    		$("#preberiMeritveVitalnihZnakovSporocilo").html("<span class='obvestilo label label-warning fade-in'>Ni podatkov!</span>");
+					    	}
+
+					    },
+					    error: function(err) {
+					    	$("#preberiMeritveVitalnihZnakovSporocilo").html("<span class='obvestilo label label-danger fade-in'>Napaka '" + JSON.parse(err.responseText).userMessage + "'!");
+							console.log(JSON.parse(err.responseText).userMessage);
+					    }
+					});
+				}
+	    	},
+			error: function(err) {
+	    		$("#preberiMeritveVitalnihZnakovSporocilo").html("<span class='obvestilo label label-danger fade-in'>Napaka '" + JSON.parse(err.responseText).userMessage + "'!");
+				console.log(JSON.parse(err.responseText).userMessage);
+	    	}
+	});
 }
 
 function switchTab(niz){
